03d8277d7df2cf2756708e7a25df719f92517ac2
anjh <anjh1@yonyou.com>
2020-01-03 18:03:46 +0800

    anker fist commit

---
+++ src/main/java/com/anker/luck_store_java/LuckStoreJavaApplication.java
@@ -0,0 +1,13 @@
+package com.anker.luck_store_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckStoreJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckStoreJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anker/luck_store_java/LuckStoreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anker.luck_store_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckStoreJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
